9cb0cf7f44858905335e3f929979c3d904fe38b1
wangchenggggdn <wangchenggggdn@163.com>
2025-01-09 17:40:41 +0800

    修改界面

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        handwriting: ['"Great Vibes"', 'cursive'],
         sans: [
           '-apple-system',
           'BlinkMacSystemFont',
